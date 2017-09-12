556235324cdb110233b85d65c8be3fb745f47344
jonaselan <jonas.elan@gmail.com>
2017-09-12 11:17:11 -0300

    Create a edit section for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -7,13 +9,29 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_url, :notice => "Signed up!"
+      redirect_to root_url; flash[:success] = "Signed up!"
     else
       render "new"
     end
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to root_path; flash[:success] = 'User was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
 private
+  def set_user
+    @user = User.find current_user.id
+  end
 
   def user_params
     params.require(:user).permit(:email, :username,
